fd7e9560b86cfb9241f005afcfc97dcad60e317f
blakehulett7 <blake.hulett7@gmail.com>
2025-04-22 09:21:13 -0500

    adding an endpoint to process uploaded docs

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	fmt.Println("Jesus is Lord!")
+	const version = "v1"
 
 	router := http.NewServeMux()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, r.URL.Path[1:]) })
+	router.HandleFunc(fmt.Sprintf("/%v/upload"), func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("upload endpoint hit")
+	})
 
 	server := &http.Server{
 		Addr:    ":8080",
