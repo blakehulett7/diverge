f752c423855f4f89051427ac743277214442bc6e
blakehulett7 <blake.hulett7@gmail.com>
2025-04-22 08:34:16 -0500

    taking a checkpoint that i forgot to take last night

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Jesus is Lord!")
+
+const uploadForm = document.getElementById("resume-input")
+console.log(uploadForm.files)
+
+uploadForm.addEventListener("change", function() {
+    console.log(uploadForm.submit())
+})
