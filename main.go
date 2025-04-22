63c2193dd926edd007e037a8b6f78e0a6dc809c1
blakehulett7 <blake.hulett7@gmail.com>
2025-04-22 09:32:09 -0500

    i think this is the key

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	fmt.Println("Jesus is Lord!")
-	const version = "v1"
+	version := "v1"
 
 	router := http.NewServeMux()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, r.URL.Path[1:]) })
-	router.HandleFunc(fmt.Sprintf("/%v/upload"), func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(fmt.Sprintf("POST /%v/upload", version), func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("upload endpoint hit")
 	})
 
