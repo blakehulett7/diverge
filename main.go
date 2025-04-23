22d399ec173193941ed96a31637c4e4d2e83897a
blakehulett7 <blake.hulett7@gmail.com>
2025-04-22 19:09:04 -0500

    finished the move

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -14,24 +11,7 @@ func main() {
 
 	router := http.NewServeMux()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, r.URL.Path[1:]) })
-	router.HandleFunc(fmt.Sprintf("POST /%v/upload", version), func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("upload endpoint hit")
-
-		file, header, err := r.FormFile("resume")
-		defer file.Close()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("filename: %v\n", header.Filename)
-
-		echo, err := os.Create("echo/test.pdf")
-		defer echo.Close()
-
-		_, err = io.Copy(echo, file)
-
-		http.Redirect(w, r, "/redirect.html", http.StatusSeeOther)
-	})
+	router.HandleFunc(fmt.Sprintf("POST /%v/upload", version), handle_file_upload)
 
 	server := &http.Server{
 		Addr:    ":8080",
