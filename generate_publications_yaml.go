aa5b6a62d4e7260e602e20e325ffac96dc8beba5
blakehulett7 <blake.hulett7@gmail.com>
2025-04-25 11:23:21 -0500

    finished cleaning the yaml generator

--- generate_publications_yaml.go
+++ generate_publications_yaml.go
@@ -73,25 +73,25 @@ func generate_publications_yaml(pdf []byte) {
 			categories: ["image-processing"]
 			tags: ["Image Processing", "Computer Vision"]
 
-        - title: An Example Paper on Security
-        publishedIn:
-            name: 2020 IEEE Region Symposium (TENSYMP)
-            date: 7 June 2020
-            url: https://example.com
-        authors:
-        - name: Dr. Madman
-            url: https://example.com
-        - name: Dr. Lessmad
-            url: https://example.com
-        - name: Dr. Moremad
-            url: https://example.com
-        - name: Dr. Goodman
-            url: https://example.com
-        paper:
-            summary: Dolor ad cupidatat pariatur nulla ipsum ex ullamco nisi anim ullamco. Dolore elit esse in exercitation minim aliqua. Amet velit incididunt magna laboris. Proident reprehenderit deserunt ad officia duis dolor. Tempor nostrud ullamco ullamco sint deserunt cupidatat irure minim consectetur nulla adipisicing sunt.
-            url: https://example.com
-        categories: ["security"]
-        tags: ["Machine Learning", "Autonomous Driving", "Machine Vision"]
+        -   title: An Example Paper on Security
+			publishedIn:
+				name: 2020 IEEE Region Symposium (TENSYMP)
+				date: 7 June 2020
+				url: https://example.com
+			authors:
+			-   name: Dr. Madman
+				url: https://example.com
+			-   name: Dr. Lessmad
+				url: https://example.com
+			-   name: Dr. Moremad
+				url: https://example.com
+			-   name: Dr. Goodman
+				url: https://example.com
+			paper:
+				summary: Dolor ad cupidatat pariatur nulla ipsum ex ullamco nisi anim ullamco. Dolore elit esse in exercitation minim aliqua. Amet velit incididunt magna laboris. Proident reprehenderit deserunt ad officia duis dolor. Tempor nostrud ullamco ullamco sint deserunt cupidatat irure minim consectetur nulla adipisicing sunt.
+				url: https://example.com
+			categories: ["security"]
+			tags: ["Machine Learning", "Autonomous Driving", "Machine Vision"]
 
         Here are some further instructions:
         - No other sections should be added to this yaml
