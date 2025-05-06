bf424e46ae11814c28609d99040b16c8828020f8
blakehulett7 <blake.hulett7@gmail.com>
2025-05-06 15:08:34 -0500

    more times.. better way?

--- generate_accomplishments_yaml.go
+++ generate_accomplishments_yaml.go
@@ -8,7 +8,7 @@ import (
 	"google.golang.org/genai"
 )
 
-func generate_accomplishments_yaml(pdf []byte) {
+func generate_accomplishments_yaml(pdf []byte, site_path string) {
 	response := ask_gemini(pdf, genai.NewPartFromText(`
 		Can you please map the accomplishments section of the uploaded resume to the following yaml format:
 
@@ -63,7 +63,8 @@ func generate_accomplishments_yaml(pdf []byte) {
 
 	cleaned := clean_response(response)
 
-	err := os.WriteFile("tmp/accomplishments.yaml", []byte(cleaned), fs.ModePerm)
+	file_path := fmt.Sprintf("%v/data/en/sections/accomplishments.yaml", site_path)
+	err := os.WriteFile(file_path, []byte(cleaned), fs.ModePerm)
 	if err != nil {
 		fmt.Println(err)
 	}
