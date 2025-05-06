22f42ce6ffb33c95b4bce991e33bd6ddb5367f04
blakehulett7 <blake.hulett7@gmail.com>
2025-05-06 11:46:26 -0500

    pared down the list of possible sections

--- section_getter_map.go
+++ section_getter_map.go
@@ -1,13 +1,11 @@
 package main
 
 var section_getter_map = map[string]func([]byte){
-	"about":           generate_about_yaml,
-	"accomplishments": nil,
-	"achievements":    nil,
-	"author":          generate_author_yaml,
-	"education":       generate_education_yaml,
-	"experiences":     generate_experiences_yaml,
-	"projects":        generate_projects_yaml,
-	"publications":    generate_publications_yaml,
-	"skills":          generate_skills_yaml,
+	"about":        generate_about_yaml,
+	"author":       generate_author_yaml,
+	"education":    generate_education_yaml,
+	"experiences":  generate_experiences_yaml,
+	"projects":     generate_projects_yaml,
+	"publications": generate_publications_yaml,
+	"skills":       generate_skills_yaml,
 }
